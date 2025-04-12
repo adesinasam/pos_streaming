54ccab9f17c76d9962c8c45a49f0ed576ed1bfdb
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-04-12 06:34:34 +0100

    Update remote_sync.py

--- pos_streaming/pos_streaming/api/remote_sync.py
+++ pos_streaming/pos_streaming/api/remote_sync.py
@@ -5,7 +5,7 @@ import json
 import logging
 from frappe.utils import get_request_site_address, now
 
-logger = frappe.get_logger("pos_streaming")
+logger = frappe.logger("pos_streaming")
 
 REMOTE_URL = frappe.conf.get('remote_url')
 API_KEY = frappe.conf.get('remote_api_key')
