45d9ed5ff810b4ac354a339df40a1a7e6127ac6f
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-04-12 05:35:12 +0100

    Update remote_sync.py

--- pos_streaming/pos_streaming/api/remote_sync.py
+++ pos_streaming/pos_streaming/api/remote_sync.py
@@ -2,7 +2,7 @@ import frappe
 import requests
 import logging
 from frappe.utils import get_request_site_address
-from frappe.secrets import get as get_secret
+from frappe.core.doctype.secrets_manager.secrets_manager import get_secret
 
 logger = frappe.logger("pos_streaming")
 
