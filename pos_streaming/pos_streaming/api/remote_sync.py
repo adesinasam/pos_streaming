139a0f44c8841d583ef9237a5f80ff2e8734ff7f
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2025-04-12 05:27:36 +0100

    Update remote_sync.py

--- pos_streaming/pos_streaming/api/remote_sync.py
+++ pos_streaming/pos_streaming/api/remote_sync.py
@@ -2,6 +2,7 @@ import frappe
 import requests
 import logging
 from frappe.utils import get_request_site_address
+from frappe.secrets import get as get_secret
 
 logger = frappe.logger("pos_streaming")
 
@@ -9,9 +10,9 @@ logger = frappe.logger("pos_streaming")
 # API_KEY = frappe.conf.get('remote_api_key')
 # API_SECRET = frappe.conf.get('remote_api_secret')
 
-REMOTE_URL = frappe.get_secret('remote_url')
-API_KEY = frappe.get_secret('remote_api_key')
-API_SECRET = frappe.get_secret('remote_api_secret')
+REMOTE_URL = get_secret('remote_url')
+API_KEY = get_secret('remote_api_key')
+API_SECRET = get_secret('remote_api_secret')
 
 headers = {
     'Content-Type': 'application/json'
